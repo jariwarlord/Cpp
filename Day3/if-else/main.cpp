160087bd2cbf61c2e80569ce8ce854d311bb97c5
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2022-11-16 14:53:03 +0300

    Add files via upload

---
+++ Day3/if-else/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age = 132;
+    if(age>60) {
+        if(age > 100) {
+            cout << "why are you still alive" << endl;
+        }
+    }else{
+        cout << "get a job!!" << endl;
+    }
+
+    return 0;
+}
---
+++ Day3/if2/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x = 10;
+   int y = 43;
+   /*
+   if(x=10){
+    cout << "omg i am fine" << endl;
+   }*/
+   if(x >= y){
+    cout << "omg i am fine" << endl;
+   }
+
+    return 0;
+}
