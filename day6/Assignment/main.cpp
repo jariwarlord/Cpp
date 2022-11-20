388f24f04f7f41442adb4af4583d2248b7243a7c
Berke Dalar <57264829+jariwarlord@users.noreply.github.com>
2022-11-20 15:35:38 +0300

    Add files via upload

---
+++ day6/Assignment/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 10;
+
+    cout << x++ << endl;
+    cout << x << endl;
+    cout << ++x << endl; // öncesinde yazarsak direkt variableý deðiþtirir.
+    cout << x << endl;
+
+}
---
+++ day6/for Loops/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    for(int x = 1; x <= 101; x++){
+        cout << x << endl;
+    }
+    */
+    for(int x = 5; x <= 50; x+=5){
+        cout << x << endl;
+    }
+}
